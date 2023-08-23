4178a4eb6a4d98c119f6ad0add0b816a3a0e1b7c
chiahao <hao.wu1980@gmail.com>
2023-08-23 10:01:25 +0800

    add ntnu banner

--- frontend/src/NTNUBanner.js
+++ frontend/src/NTNUBanner.js
@@ -1,26 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import './personForm.scss';
 import logo_ntnu from './images/logo-ntnu.png';
 
 export default function NTNUBanner() {
     return (
         <>
-            <Navbar className="above-navbar bg-body-tertiary" bg="dark" data-bs-theme="dark">
+            <Navbar fluid data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand href="#home">
                         <img
                             alt="ntnu_logo"
                             src={logo_ntnu}
-                            className="d-inline-block align-top"
+                            className="d-inline-block align-top navbar_right_border"
                             height="36"
                         />{' '}
-                        人事室
+                        <span>{' '}人事室</span>
                     </Navbar.Brand>
                 </Container>
             </Navbar>
