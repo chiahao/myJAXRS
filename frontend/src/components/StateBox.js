cd4fda947217fc09e9bcca7d8dd2d70a782fb54f
chiahao <hao.wu1980@gmail.com>
2023-09-01 08:11:31 +0800

    add StateBox

--- frontend/src/components/StateBox.js
+++ frontend/src/components/StateBox.js
@@ -1,15 +1,31 @@
-import { Box, useTheme} from "@mui/material";
+import {Box, Typography, useTheme} from "@mui/material";
 import { tokens } from "../theme";
 
-const StateBox = () => {
+const StateBox = ({title, icon, amount}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     return (
         <Box width="100%" m="0 30px">
             <Box display="flex" justifyContent="space-between">
+                <Box display="flex" sx={{ verticalAlign: 'center'}}>
+                    <Box>
+                        {icon}
+                        <Typography
+                            variant="h4"
+                            sx={{ color: colors.grey[100] }}
+                        >
+                            {title}
+                        </Typography>
+                    </Box>
+                </Box>
                 <Box>
-
+                    <Typography
+                        variant="h2"
+                        sx={{ display: "block", color: colors.grey[100] }}
+                    >
+                        {amount}
+                    </Typography>
                 </Box>
             </Box>
         </Box>
--- frontend/src/scenes/dashboard/index.js
+++ frontend/src/scenes/dashboard/index.js
@@ -1,5 +1,8 @@
 import {Box, Typography, useTheme} from "@mui/material";
 import { tokens } from "../../theme";
+import StateBox from "../../components/StateBox";
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 const Dashboard = () => {
     const theme = useTheme();
@@ -10,7 +13,7 @@ const Dashboard = () => {
           {/* HEADER */}
           <Box display="flex" justifyContent="space-between" alignItems="center">
               <Box mb="30px">
-                  <Typography variant="h2" color={colors.grey[100]}>
+                  <Typography variant="h2" color={colors.grey[100]} sx={{ m: "0 0 10px 0"}}>
                       系統總覽
                   </Typography>
                   <Typography variant="h5" color={colors.greenAccent[400]}>
@@ -32,8 +35,57 @@ const Dashboard = () => {
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
+                  border="1"
+                  borderColor="green"
               >
-
+                  <StateBox
+                      title="待簽核"
+                      icon={
+                        <NoteAltIcon
+                            sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                        />
+                      }
+                      amount={5}
+                  >
+                  </StateBox>
+              </Box>
+              <Box
+                  gridColumn="span 4"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  border="1"
+                  borderColor="green"
+              >
+                  <StateBox
+                      title="本月已簽核"
+                      icon={
+                          <InventoryIcon
+                              sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                          />
+                      }
+                      amount={0}
+                  >
+                  </StateBox>
+              </Box>
+              <Box
+                  gridColumn="span 4"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  border="1"
+                  borderColor="green"
+              >
+                  <StateBox
+                      title="全部已簽核"
+                      icon={
+                          <InventoryIcon
+                              sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                          />
+                      }
+                      amount={'99+'}
+                  >
+                  </StateBox>
               </Box>
           </Box>
       </Box>
